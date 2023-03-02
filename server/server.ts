32a5a9f1d72747d648aae157c05c22cd1402ef63
PawelWrzesinski <pawel.wrzesinski87@gmail.com>
2023-03-02 10:47:57 +0100

    Added serving contacts through rest api with basic express server.

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+import { getAllContacts, getContactByFirstNameAndLastName } from './get-contacts.route';
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.use(bodyParser.json());
+
+app.route('/api/contacts').get(getAllContacts);
+
+app.route('/api/contacts/:firstName/:lastName').get(getContactByFirstNameAndLastName);
+
+// app.route();
+
+
+const port = 9001;
+
+const httpServer = app.listen(port, () => {
+    console.log(`HTTP REST API SERVER running at http:localhost:${port}`);
+});
